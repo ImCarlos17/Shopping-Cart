2337c266a078b96eef41b26df801b3a5cb413842
ImCarlos17 <carlosmba96@gmail.com>
2022-08-25 21:09:58 -0400

    update cart

--- src/component/ProductContainer.js
+++ src/component/ProductContainer.js
@@ -1,7 +1,32 @@
 import React from "react";
+const ProductContainer = ({ product, addToCart, delFromCart }) => {
+  const { name, price, img, quantity, id } = product;
 
-const ProductContainer = () => {
-  return <div></div>;
+  return (
+    <div className="flex w-36 h-36 gap-6">
+      <img src={img} />
+
+      <div className="flex flex-col items-center grid-rows-3 grid-cols-1 w-36 h-36 gap-6">
+        <h1>{name}</h1>
+        <span>${price * quantity}.00</span>
+        <div className="flex gap-2 ">
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+            onClick={() => addToCart(id)}
+          >
+            +
+          </button>
+          <span>{quantity}</span>
+          <button
+            onClick={() => delFromCart(id)}
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+          >
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductContainer;
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Card from "./Card";
+import ProductContainer from "./ProductContainer";
+import { addToCard, delFromCart } from "../actions/shoppingActions";
 
 const Products = () => {
   const state = useSelector((state) => state.reducer);
   const { cart } = state.shopping;
+  const dispatch = useDispatch();
+
   return (
-    <div className="flex flex-col gap-y-6 border-black border-2 max-w-max max-h-96 overflow-auto">
-      {cart && cart.map((item) => <img src={item.img} />)}
+    <div className="flex flex-col gap-y-6 border-black border-2 w-96 max-h-96 overflow-auto">
+      {cart &&
+        cart.map((item) => (
+          <ProductContainer
+            product={item}
+            key={item.id}
+            addToCart={() => dispatch(addToCard(item.id))}
+            delFromCart={() => dispatch(delFromCart(item.id))}
+          />
+        ))}
     </div>
   );
 };
